6e17b2eb8f567b3abd6e85d504f78cff992d1f46
tashi thompson <tosh@tashis-MacBook-Pro.local>
2024-09-25 15:16:20 -0400

    completed ShoppingList app

--- src/main/java/com/pluralsight/ShoppingList.java
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -2,12 +2,19 @@ package com.pluralsight;
 
 public class ShoppingList {
     public static void main(String[] args) {
-        //print a shopping list
+        //print a shopping list with 10 items
         System.out.println("Shopping list:");
         System.out.println("______________");
         System.out.println("Apples");
         System.out.println("Pear");
-        
+        System.out.println("Kiwi");
+        System.out.println("Banana");
+        System.out.println("Milk");
+        System.out.println("Juice");
+        System.out.println("Nuts");
+        System.out.println("Cheese");
+        System.out.println("Yogurt");
+        System.out.println("Bread");
 
     }
 }
