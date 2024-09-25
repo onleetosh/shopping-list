7288ab0ab380ad75c27a9e8a677edac9ed2341ae
tashi thompson <tosh@tashis-MacBook-Pro.local>
2024-09-25 15:10:50 -0400

    completed ShoppingList app

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        //print a shopping list
+        System.out.println("Shopping list:");
+        System.out.println("______________");
+        System.out.println("Apples");
+        System.out.println("Pear");
+        
+
+    }
+}
